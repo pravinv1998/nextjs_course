b6baac3154c63bc3c216c5a89648f90dd071df19
pravinv1998 <pravin.wargad1998@gmail.com>
2021-06-18 16:23:31 +0530

    last change

--- pages/details/index.js
+++ pages/details/index.js
@@ -1,7 +1,14 @@
+import Link from "next/link";
+
 const index = () => {
   return (
     <div>
       <h1>this is details page</h1>
+      <Link href="/details/this is dynamic routing">
+        <a>
+          <h2>this is routing link</h2>
+        </a>
+      </Link>
     </div>
   );
 };
